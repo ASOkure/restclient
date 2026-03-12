1b4ca1b447f33493d953ff773ec91981d97071d2
akanimo sam okure <akan.okure@gmail.com>
2026-03-12 16:54:16 +0000

    first commit

---
+++ src/main/java/com/aso/resthttpclient/ResthttpclientApplication.java
@@ -0,0 +1,13 @@
+package com.aso.resthttpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResthttpclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResthttpclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aso/resthttpclient/ResthttpclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aso.resthttpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResthttpclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
